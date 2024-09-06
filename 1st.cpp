a57de48f3575eef266ec2f56607c864b8d4ae9d7
RAKESH NISHAD <nishadrakesh1230@gmail.com>
2024-09-06 22:10:40 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int a,b;
+    cin>>a;
+    cout<<"Enter a : "<<a<<endl;
+    cin>>b;
+    cout<<"Enter a : "<<b<<endl;
+    
+    cout<<"Sum is : "<<a+b;
+}
